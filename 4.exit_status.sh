0c68cd2dd04262c236ffc464b907401244be8987
SATISH REDDY SATTI <satishdevops3789@gmail.com>
2026-02-28 16:44:16 +0530

    Update 4.exit_status.sh

--- 4.exit_status.sh
+++ 4.exit_status.sh
@@ -10,3 +10,18 @@ echo Function Exit status - $?
 echo Hello
 exit 100
 echo Bye
+
+
+# One-line difference
+return → exits a function
+exit → exits the entire script
+
+
+#!/bin/bash
+
+if [ ! -f /etc/passwd ]; then
+  echo "Critical file missing"
+  exit 1
+fi
+
+echo "This line will run only if file exists"
